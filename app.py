254cd51c98534ea3e70c9471d95ed1e13ded5858
gurur <gururum1973@hotmail.com>
2025-04-25 04:13:25 +0300

    test commit

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask, jsonify
 import os
-import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
     try:
-        conn = psycopg2.connect(
-            host=os.environ.get("DB_HOST"),
-            dbname=os.environ.get("DB_NAME"),
-            user=os.environ.get("DB_USER"),
-            password=os.environ.get("DB_PASS"),
-            connect_timeout=3,
-        )
         return jsonify({"message": "Connected to PostgreSQL successfully!"})
     except Exception as e:
         return jsonify({"error": str(e)})
