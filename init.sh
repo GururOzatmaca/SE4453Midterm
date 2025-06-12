c4ff76cda76388af458bcc5dbe2839fa94efe0f2
gurur <gururum1973@hotmail.com>
2025-06-13 02:02:46 +0300

    modified init

--- init.sh
+++ init.sh
@@ -3,7 +3,5 @@
 # Start SSH service
 service ssh start
 
-# Start Python web app (adjust if different)
-# For example, FastAPI with Uvicorn:
-uvicorn app:app --host 0.0.0.0 --port 8000
-
+# Start Flask app
+python app.py
