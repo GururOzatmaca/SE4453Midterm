47408de1a0c62550c83e2e78e524f03e6f64eb0c
gurur <gururum1973@hotmail.com>
2025-06-12 23:36:46 +0300

    Removed blocking part

--- app.py
+++ app.py
@@ -22,6 +22,4 @@ def hello():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))  # Azure will inject PORT env var
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host="0.0.0.0", port=8000)
